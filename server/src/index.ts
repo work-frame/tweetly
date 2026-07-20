6ef60b14043f6f85cc5b1e89aa072b74fe399a6f
work-frame <uperfect2008@gmail.com>
2026-07-20 22:13:42 +0100

    Temporarily open CORS to diagnose Vercel connection issue

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,10 +14,7 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.use(cors({
-  origin: 'https://tweetly-rose.vercel.app',
-  credentials: true,
-}))
+app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
